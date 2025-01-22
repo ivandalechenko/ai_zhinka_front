1a191d920e0a55e318b01375b098e6f2b526fbc9
logikmars <dima76673@gmail.com>
2025-01-23 00:03:57 +0200

    fix

--- src/components/Decor/Decor.tsx
+++ src/components/Decor/Decor.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import './Decor.scss';
 const Decor: React.FC = () => {
+
+    const [offset, setOffset] = useState({ x: 0});
+
+    useEffect(() => {
+        const handleMouseMove = (e: MouseEvent) => {
+            const { clientX } = e;
+            const centerX = window.innerWidth / 2;
+            const offsetX = (clientX - centerX) * 0.02;
+            setOffset({ x: offsetX});
+        };
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []);
+
+
     return (
-        <div className='Decor free_img'>
+        <div className='Decor free_img' 
+             style={{
+             transform: `translate(${offset.x}px`,
+             transition: "transform 0.1s ease-out",
+        }}>
             <img src="/img/decor/decor.svg" alt="" />
         </div>
     )
