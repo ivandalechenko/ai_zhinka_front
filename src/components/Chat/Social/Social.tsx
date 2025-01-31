8967f0c67cfabf86875c8d03ae5ffa9c7c2cf37d
ivandalechenko <ivandalechenko@gmail.com>
2025-01-31 17:18:46 +0200

    dex

--- src/components/Chat/Social/Social.tsx
+++ src/components/Chat/Social/Social.tsx
@@ -3,7 +3,7 @@ import './Social.scss';
 const Social: React.FC = () => {
     return (
         <div className='Social'>
-            <a href="#" target='_blank'><img src="/img/social/dexScreener.svg" alt="" className='Social__img' /></a>
+            <a href="https://dexscreener.com/solana/8spkcaaq1yy8bhrpvmzdn3fvegmkvfqcv4kegs6pyq4j" target='_blank'><img src="/img/social/dexScreener.svg" alt="" className='Social__img' /></a>
             <a href="https://x.com/AMYXBTXYZ" target='_blank'><img src="/img/social/twitter.svg" alt="" className='Social__img' /></a>
             <a href="https://t.me/amyxbt" target='_blank'><img src="/img/social/tg.svg" alt="" className='Social__img' /></a>
         </div>
