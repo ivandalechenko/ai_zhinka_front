231112fe5db9497e640396dca15886699c380e44
ivandalechenko <ivandalechenko@gmail.com>
2025-01-31 17:21:31 +0200

    fix

--- src/components/Chat/Social/Social.tsx
+++ src/components/Chat/Social/Social.tsx
@@ -3,7 +3,7 @@ import './Social.scss';
 const Social: React.FC = () => {
     return (
         <div className='Social'>
-            <a href="https://dexscreener.com/solana/8spkcaaq1yy8bhrpvmzdn3fvegmkvfqcv4kegs6pyq4j" target='_blank'><img src="/img/social/dexScreener.svg" alt="" className='Social__img' /></a>
+            <a href="https://dexscreener.com/solana/ftxrgsf7aznldoigejbhajpj5j5a77lt5aeahs7pump" target='_blank'><img src="/img/social/dexScreener.svg" alt="" className='Social__img' /></a>
             <a href="https://x.com/AMYXBTXYZ" target='_blank'><img src="/img/social/twitter.svg" alt="" className='Social__img' /></a>
             <a href="https://t.me/amyxbt" target='_blank'><img src="/img/social/tg.svg" alt="" className='Social__img' /></a>
         </div>
